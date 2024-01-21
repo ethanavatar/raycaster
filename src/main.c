1c83c353b2d4e713dfc391a5f8e5512b89dc5b89
Ethan Evans <ethanalexevans@gmail.com>
2024-01-21 00:17:28 -0800

    they leak through the  w a l l s

--- src/main.c
+++ src/main.c
@@ -2,12 +2,20 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
 #include <stdbool.h>
 
-const int tileSize = 32;
-const int lineWidth = 10;
+#define PI 3.14159265358979323846f
 
-const int map[10][10] = {
+const int screenWidth = 1600;
+const int screenHeight = 1200;
+
+const int canvasPixelScale = 2;
+
+const int tileSize = 64;
+#define ROOM_COLUMNS 10
+#define ROOM_ROWS 10
+const int map[ROOM_COLUMNS][ROOM_ROWS] = {
     { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
     { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
     { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
@@ -20,152 +28,218 @@ const int map[10][10] = {
     { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
 };
 
+const float playerMoveSpeed = 3.0f;
+const float playerRotationSpeed = 3.0f;
 struct Player {
     Vector2 position;
     Vector2 direction;
-    int rotation; //0 -> 360
+    float rotation; // in radians
+} player = {
+    .position = { 3.0f, 3.0f },
+    .direction = {
+        .x = 1.0f, // cosf(0.0f)
+        .y = 0.0f  // sinf(0.0f)
+    },
+    .rotation = 0.0f
 };
 
-struct Player player = {
-    .position = { 5.0f, 5.0f },
-    .direction = { 0.0f, 0.0f },
-    .rotation = 0
-};
+float frameDistances[60] = { 0 };
 
-void PlayerUpdate(void) {
+void UpdatePlayer(float deltaTime) {
     if (IsKeyDown(KEY_W)) {
-        player.position.x += player.direction.x * 0.1f;
-        player.position.y += player.direction.y * 0.1f;
+        player.position.x += player.direction.x * playerMoveSpeed * deltaTime;
+        player.position.y += player.direction.y * playerMoveSpeed * deltaTime;
     }
-
     if (IsKeyDown(KEY_S)) {
-        player.position.x -= player.direction.x * 0.1f;
-        player.position.y -= player.direction.y * 0.1f;
+        player.position.x -= player.direction.x * playerMoveSpeed * deltaTime;
+        player.position.y -= player.direction.y * playerMoveSpeed * deltaTime;
     }
-
     if (IsKeyDown(KEY_A)) {
-        player.rotation -= 5;
-    }
+        player.rotation -= playerRotationSpeed * deltaTime;
 
+        if (player.rotation < 0.0f) {
+            player.rotation += 2.0f * PI;
+        }
+
+        player.direction.x = cosf(player.rotation);
+        player.direction.y = sinf(player.rotation);
+    }
     if (IsKeyDown(KEY_D)) {
-        player.rotation += 5;
-    }
+        player.rotation += playerRotationSpeed * deltaTime;
 
-    if (player.rotation > 360) {
-        player.rotation = 0;
-    }
-
-    if (player.rotation < 0) {
-        player.rotation = 360;
-    }
-
-    player.direction.x = cos(player.rotation * (PI / 180));
-    player.direction.y = sin(player.rotation * (PI / 180));
-}
-
-void Render2DMap(void) {
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
-
-            int tileX = x * tileSize;
-            int tileY = y * tileSize;
-
-            if (map[y][x] == 1) {
-                DrawRectangle(tileX, tileY, tileSize, tileSize, WHITE);
-            }
+        if (player.rotation > 2.0f * PI) {
+            player.rotation -= 2.0f * PI;
         }
-    }
-}
-
-void Render2DPlayer(void) {
-    DrawCircle(player.position.x * tileSize + tileSize / 2, player.position.y * tileSize + tileSize / 2, 5, RED);
-}
-
-bool isHit(Vector2 endPosition) {
-    for (int row = 0; row < 10; ++row) {
-        for (int col = 0; col < 10; ++col) {
-
-            if (map[row][col] == 0) {
-                continue;
-            }
-
-            int tileX = col * tileSize;
-            int tileY = row * tileSize;
-
-            Rectangle tileRect = { tileX, tileY, tileSize, tileSize };
-            if (CheckCollisionPointRec(endPosition, tileRect)) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-void Draw2DRays(int fov) {
-    int maxRayLength = 300;
-    
-    // send rays out for each degree of the fov
-    for (int rayAngle = -fov / 2; rayAngle < fov / 2; ++rayAngle) {
-        // calculate the direction of the ray based on the player's rotation
-        Vector2 rayDirection = { cos((player.rotation + rayAngle) * (PI / 180)), sin((player.rotation + rayAngle) * (PI / 180)) };
-        Vector2 rayPosition = { player.position.x * tileSize + tileSize / 2, player.position.y * tileSize + tileSize / 2 };
-
-        Vector2 rayEnd;
-        for (int rayLength = 0; rayLength < maxRayLength; ++rayLength) {
-            rayEnd.x = rayPosition.x + rayDirection.x * rayLength;
-            rayEnd.y = rayPosition.y + rayDirection.y * rayLength;
-
-            if (isHit(rayEnd)) {
-                break;
-            }
-        }
-
-        DrawLineEx(rayPosition, rayEnd, 1, GREEN);
-    }
-}
-
-void Draw3dViewport(int fov) {
-    int maxRayLength = 200;
-    
-    // send rays out for each degree of the fov
-    for (int rayAngle = -fov / 2; rayAngle < fov / 2; ++rayAngle) {
-        // calculate the direction of the ray based on the player's rotation
-        Vector2 rayDirection = { cos((player.rotation + rayAngle) * (PI / 180)), sin((player.rotation + rayAngle) * (PI / 180)) };
-        Vector2 rayPosition = { player.position.x * tileSize + tileSize / 2, player.position.y * tileSize + tileSize / 2 };
         
-        Vector2 rayEnd;
-        for (int rayLength = 0; rayLength < maxRayLength; ++rayLength) {
-            rayEnd.x = rayPosition.x + rayDirection.x * rayLength;
-            rayEnd.y = rayPosition.y + rayDirection.y * rayLength;
+        player.direction.x = cosf(player.rotation);
+        player.direction.y = sinf(player.rotation);
+    }
+}
 
-            if (isHit(rayEnd)) {
-                float distance = sqrt(pow(rayEnd.x - rayPosition.x, 2) + pow(rayEnd.y - rayPosition.y, 2));
-                Color wallColor = { 255, 255, 255, 255 - (distance / maxRayLength) * 255 };
+void DrawPlayer() {
+    Vector2 playerPosition = {
+        player.position.x * tileSize,
+        player.position.y * tileSize
+    };
+    DrawCircleV(playerPosition, 10, BLUE);
+    DrawLineEx(playerPosition, (Vector2){
+        playerPosition.x + player.direction.x * 30,
+        playerPosition.y + player.direction.y * 30
+    }, 2, RED);
+}
 
-                // the rectangle is shorter the further away the wall is
-                DrawRectangle(
-                    rayAngle * lineWidth + 400 - lineWidth / 2,
-                    300 - (maxRayLength / distance) * 100 / 2,
-                    lineWidth,
-                    (maxRayLength / distance) * 100,
-                    wallColor
-                );
+void DrawMap() {
+    for (int x = 0; x < ROOM_COLUMNS; x++) {
+        for (int y = 0; y < ROOM_ROWS; y++) {
+            Rectangle wall = {
+                x * tileSize + 1,
+                y * tileSize + 1,
+                tileSize - 2,
+                tileSize - 2
+            };
+            Color color = map[x][y] == 1 ? RAYWHITE : BLACK;
+            DrawRectangleRec(wall, color);
+        }
+    }
+}
 
+float distance(Vector2 a, Vector2 b) {
+    return sqrtf(powf(a.x - b.x, 2) + powf(a.y - b.y, 2));
+}
+
+void DrawRays2D() {
+    // The player's rotation is in radians
+    float rayAngle = player.rotation - PI / 6.0f; // 30 degrees
+    Vector2 ray = { 0 };
+    Vector2 rayStep = { 0 };
+
+    for (int rayIndex = 0; rayIndex < 60; ++rayIndex) {
+        Vector2 shortestRay = { 0 };
+        float shortestDistance = FLT_MAX;
+
+        // horizontal ray-grid intersection
+        if (rayAngle > PI) {
+            ray.y = ((int) player.position.y) * tileSize - 1;
+            rayStep.y = -tileSize;
+        } else {
+            ray.y = ((int) player.position.y) * tileSize + tileSize;
+            rayStep.y = tileSize;
+        }
+
+        ray.x = player.position.x * tileSize + (ray.y - player.position.y * tileSize) / tanf(rayAngle);
+        rayStep.x = rayStep.y / tanf(rayAngle);        
+
+        // looking directly left or right
+        if (rayAngle == PI / 2.0f || rayAngle == 3.0f * PI / 2.0f) {
+            ray.x = player.position.x * tileSize;
+            rayStep.x = 0.0f;
+        }
+
+        // ray marching
+        while (ray.x >= 0 && ray.x < ROOM_COLUMNS * tileSize && ray.y >= 0 && ray.y < ROOM_ROWS * tileSize) {
+            int mapX = (int) (ray.x / tileSize);
+            int mapY = (int) (ray.y / tileSize);
+
+            if (map[mapX][mapY] == 1) {
+                float distanceToWall = distance(player.position, ray);
+                if (distanceToWall < shortestDistance) {
+                    shortestDistance = distanceToWall;
+                    shortestRay = ray;
+                }
                 break;
             }
+
+            ray.x += rayStep.x;
+            ray.y += rayStep.y;
         }
+
+        // vertical ray-grid intersection
+        if (rayAngle > PI / 2.0f && rayAngle < 3.0f * PI / 2.0f) {
+            ray.x = ((int) player.position.x) * tileSize - 1;
+            rayStep.x = -tileSize;
+        } else if (rayAngle < PI / 2.0f || rayAngle > 3.0f * PI / 2.0f) {
+            ray.x = ((int) player.position.x) * tileSize + tileSize;
+            rayStep.x = tileSize;
+        }
+
+        ray.y = player.position.y * tileSize + (ray.x - player.position.x * tileSize) * tanf(rayAngle);
+        rayStep.y = rayStep.x * tanf(rayAngle);
+
+        if (rayAngle == 0.0f || rayAngle == PI) {
+            ray.y = player.position.y * tileSize;
+            rayStep.y = 0.0f;
+        }
+
+        // ray marching
+        while (ray.x >= 0 && ray.x < ROOM_COLUMNS * tileSize && ray.y >= 0 && ray.y < ROOM_ROWS * tileSize) {
+            int mapX = (int) (ray.x / tileSize);
+            int mapY = (int) (ray.y / tileSize);
+
+            if (map[mapX][mapY] == 1) {
+                float distanceToWall = distance(player.position, ray);
+                if (distanceToWall < shortestDistance) {
+                    shortestDistance = distanceToWall;
+                    shortestRay = ray;
+                }
+                break;
+            }
+
+            ray.x += rayStep.x;
+            ray.y += rayStep.y;
+        }
+
+        DrawLineEx((Vector2) {
+            player.position.x * tileSize,
+            player.position.y * tileSize
+        }, (Vector2) {
+            shortestRay.x,
+            shortestRay.y
+        }, 2, GREEN);
+
+        rayAngle += PI / 180.0f;
+        frameDistances[rayIndex] = shortestDistance;
+    }
+}
+
+void Draw3D() {
+
+    float maxWallHeight = screenHeight / canvasPixelScale;
+    int width = (screenWidth / canvasPixelScale) / 60.0f;
+
+    for (int i = 0; i < 60; ++i) {
+        float distance = frameDistances[i];
+        float wallHeight = (tileSize / distance) * 1200.0f;
+
+        if (wallHeight > maxWallHeight) {
+            wallHeight = maxWallHeight;
+        }
+
+        float wallTop = (screenHeight / canvasPixelScale - wallHeight) / 2.0f;
+        float wallBottom = wallTop + wallHeight;
+
+        Color color = (Color) {
+            .r = 255.0f * (1.0f - distance / 1000.0f),
+            .g = 255.0f * (1.0f - distance / 1000.0f),
+            .b = 255.0f * (1.0f - distance / 1000.0f),
+            .a = 255
+        };
+
+        DrawRectangle(i * width, wallTop, width, wallHeight, color);
     }
 }
 
 int main(void) {
-    int screenWidth = 800; 
-    int screenHeight = 600;
 
     InitWindow(screenWidth, screenHeight, "Raycaster");
     SetTargetFPS(30);
 
-    RenderTexture2D topDownTexture = LoadRenderTexture(10 * tileSize, 10 * tileSize);
+    RenderTexture2D topDownTexture = LoadRenderTexture(tileSize * ROOM_COLUMNS + 2, tileSize * ROOM_ROWS + 1);
+    Rectangle sourceRec = { 0.0f, 0.0f, (float) topDownTexture.texture.width, (float) -topDownTexture.texture.height };
+    Rectangle destRec = { 0.0f, 0.0f, (float)topDownTexture.texture.width / 2, (float) topDownTexture.texture.height / 2 };
+
+    RenderTexture2D canvas = LoadRenderTexture(screenWidth / canvasPixelScale, screenHeight / canvasPixelScale);
+    Rectangle canvasSourceRec = { 0.0f, 0.0f, (float) canvas.texture.width, (float) -canvas.texture.height };
+    Rectangle canvasDestRec = { 0.0f, 0.0f, (float)canvas.texture.width * canvasPixelScale, (float)canvas.texture.height * canvasPixelScale };
 
     while (!WindowShouldClose()) {
 
@@ -175,29 +249,33 @@ int main(void) {
         char *title = TextFormat("Raycaster - %.2f ms", deltaTimeMs);
         SetWindowTitle(title);
 
-        PlayerUpdate();
+        UpdatePlayer(deltaTime);
+
+        for (int i = 0; i < 60; ++i) {
+            frameDistances[i] = 0;
+        }
 
         BeginTextureMode(topDownTexture);
-            ClearBackground(BLACK);
-            Render2DMap();
-            Render2DPlayer();
-            Draw2DRays(60);
+            ClearBackground(GRAY);
+            DrawMap();
+            DrawPlayer();
+            DrawRays2D();
         EndTextureMode();
 
-        // flip the texture so it's not upside down 
-        Rectangle sourceRec = { 0.0f, 0.0f, (float)topDownTexture.texture.width, (float)-topDownTexture.texture.height };
-        Rectangle destRec = { 0.0f, 0.0f, (float)topDownTexture.texture.width / 2, (float)topDownTexture.texture.height / 2 };
+        BeginTextureMode(canvas);
+            ClearBackground(RED);
+            Draw3D();
+        EndTextureMode();
 
         BeginDrawing();
             ClearBackground(BLACK);
-            Draw3dViewport(60);
-
+            DrawTexturePro(canvas.texture, canvasSourceRec, canvasDestRec, (Vector2){ 0, 0 }, 0.0f, WHITE);
             DrawTexturePro(topDownTexture.texture, sourceRec, destRec, (Vector2){ 0, 0 }, 0.0f, WHITE);
-
         EndDrawing();
     }
 
     UnloadRenderTexture(topDownTexture);
+    UnloadRenderTexture(canvas);
 
     CloseWindow();
     return 0;
